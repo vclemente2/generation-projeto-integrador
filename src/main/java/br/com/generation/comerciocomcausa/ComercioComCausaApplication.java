39a1198f864c2c9fc32c70632b65c57f2cf92c98
Vinicius Bastos <vclemente2@hotmail.com>
2023-10-04 09:18:24 -0300

    Commit inicial do projeto.

---
+++ src/main/java/br/com/generation/comerciocomcausa/ComercioComCausaApplication.java
@@ -0,0 +1,13 @@
+package br.com.generation.comerciocomcausa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComercioComCausaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComercioComCausaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/generation/comerciocomcausa/ComercioComCausaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.generation.comerciocomcausa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComercioComCausaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
