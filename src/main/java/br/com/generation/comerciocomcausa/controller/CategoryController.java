d75154fa0e7ddcb9a9d0629db0608b9709e59cf3
Angela <commercial.araujo@gmail.com>
2023-10-06 15:36:42 -0300

    tratamento de conflitos

--- src/main/java/br/com/generation/comerciocomcausa/controller/CategoryController.java
+++ src/main/java/br/com/generation/comerciocomcausa/controller/CategoryController.java
@@ -10,11 +10,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.PostMapping;
-=======
 import org.springframework.web.bind.annotation.PutMapping;
->>>>>>> 683ee9bca712a733c0667470fd9b8dfd5de6d206
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -24,10 +21,6 @@ import org.springframework.web.server.ResponseStatusException;
 import br.com.generation.comerciocomcausa.model.Category;
 import br.com.generation.comerciocomcausa.repository.CategoryRepository;
 import jakarta.validation.Valid;
-<<<<<<< HEAD
-
-=======
->>>>>>> 683ee9bca712a733c0667470fd9b8dfd5de6d206
 
 @RestController
 @RequestMapping("/categories")
@@ -49,7 +42,6 @@ public class CategoryController {
 					.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	}
 	
-<<<<<<< HEAD
 	@GetMapping("/name/{name}")
 	public ResponseEntity<List<Category>> getByName(@PathVariable String name) {
 		return ResponseEntity.ok(categoryRepository.findAllByNameContainingIgnoreCase(name));
@@ -59,7 +51,7 @@ public class CategoryController {
 	public ResponseEntity<Category> post(@Valid @RequestBody Category category) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(categoryRepository.save(category));
 	}
-=======
+
 	@PutMapping
     public ResponseEntity<Category> put(@Valid @RequestBody Category category){
         return categoryRepository.findById(category.getId())
@@ -77,9 +69,6 @@ public class CategoryController {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
 
         categoryRepository.deleteById(id);
-        
-        
     }
->>>>>>> 683ee9bca712a733c0667470fd9b8dfd5de6d206
 
 }
