8281d4008aeda9a39a145fe93e0ad8c514df2bd1
Angela <commercial.araujo@gmail.com>
2023-10-06 15:14:33 -0300

    implementação dos métodos de busca e cadastro

--- src/main/java/br/com/generation/comerciocomcausa/controller/CategoryController.java
+++ src/main/java/br/com/generation/comerciocomcausa/controller/CategoryController.java
@@ -8,11 +8,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.generation.comerciocomcausa.model.Category;
 import br.com.generation.comerciocomcausa.repository.CategoryRepository;
+import jakarta.validation.Valid;
+
 
 @RestController
 @RequestMapping("/categories")
@@ -27,10 +31,21 @@ public class CategoryController {
 			return ResponseEntity.ok(categoryRepository.findAll());
 	}
 	
-	@GetMapping
+	@GetMapping("/{id}")
 		public ResponseEntity<Category> getById(@PathVariable Long id){
 			return categoryRepository.findById(id)
 					.map(resposta -> ResponseEntity.ok(resposta))
 					.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	}
+	
+	@GetMapping("/name/{name}")
+	public ResponseEntity<List<Category>> getByName(@PathVariable String name) {
+		return ResponseEntity.ok(categoryRepository.findAllByNameContainingIgnoreCase(name));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Category> post(@Valid @RequestBody Category category) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoryRepository.save(category));
+	}
+
 }
