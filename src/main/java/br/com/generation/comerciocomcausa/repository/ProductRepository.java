808f690ec36023ce959244cb984550d8a427e99e
Lilian Almeida <lili.l2002@hotmail.com>
2023-10-09 12:00:13 -0300

    Criado método Get By Name e Delete do ProductController

--- src/main/java/br/com/generation/comerciocomcausa/repository/ProductRepository.java
+++ src/main/java/br/com/generation/comerciocomcausa/repository/ProductRepository.java
@@ -1,7 +1,13 @@
 package br.com.generation.comerciocomcausa.repository;
 
 import br.com.generation.comerciocomcausa.model.Product;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+	public List<Product> findAllByNameContainingIgnoreCase(@Param("name") String name);
+	  
 }
